a381acf49af76cb0fc77e39fa355886f23df6da5
DeepTerrorGG <daniloiliccc@gmail.com>
2025-11-06 15:01:55 +0000

    feat: implementation updates per request

--- src/components/ui/spline-viewer.tsx
+++ src/components/ui/spline-viewer.tsx
@@ -1,21 +1,12 @@
 // src/components/ui/spline-viewer.tsx
 'use client';
 
-import React from 'react';
 import Spline from '@splinetool/react-spline';
-import { cn } from '@/lib/utils';
 
-interface SplineViewerProps {
-  sceneUrl: string;
-  className?: string;
-}
-
-export default function SplineViewer({ sceneUrl, className }: SplineViewerProps) {
+export default function SplineViewer({ sceneUrl }: { sceneUrl: string }) {
   return (
-    <div className={cn("relative w-full h-full min-h-[300px] md:min-h-[500px]", className)}>
-      <Spline
-        scene={sceneUrl}
-      />
+    <div className="w-full h-full">
+      <Spline scene={sceneUrl} />
     </div>
   );
 }
